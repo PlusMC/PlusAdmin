fc744c8e096d19a76f5eccfd22a878033ae259f0
OakleyCord <66735773+HappyCord@users.noreply.github.com>
2022-01-12 12:13:51 -0500

    Add stuff to bungeeutils

--- src/main/java/org/plusmc/plusadmin/Utils/BungeeUtil.java
+++ src/main/java/org/plusmc/plusadmin/Utils/BungeeUtil.java
@@ -5,7 +5,13 @@ import com.google.common.io.ByteStreams;
 import org.plusmc.plusadmin.PlusAdmin;
 import org.bukkit.entity.Player;
 
-public class BungeeUtil {
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+public class BungeeUtil implements PluginMessageListener {
+    HashMap<String, ServerInfo> SERVER_INFO = new HashMap<>();
+    HashMap<String, PlayerList> PLAYER_LIST = new HashMap<>();
+
+
     public static void connectServer(Player player, String server) {
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF("Connect");
@@ -23,5 +29,39 @@ public class BungeeUtil {
         PlusAdmin.getInstance().getServer().sendPluginMessage(PlusAdmin.getInstance(), "BungeeCord", out.toByteArray());
     }
 
+    public static void updateServer(String server) {
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF("PlayerList");
+        out.writeUTF(server);
+        PlusAdmin.getInstance().getServer().sendPluginMessage(PlusAdmin.getInstance(), "BungeeCord", out.toByteArray());
 
+        out = ByteStreams.newDataOutput();
+        out.writeUTF("ServerIP");
+        out.writeUTF(server);
+        PlusAdmin.getInstance().getServer().sendPluginMessage(PlusAdmin.getInstance(), "BungeeCord", out.toByteArray());
+    }
+
+    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+        ByteArrayDataInput in = ByteStreams.newDataInput(message);
+        switch(channel) {
+            case "PlayerList" -> handlePlayerList(in);
+            case "ServerIP"-> handleServerIP(in);
+        };
+    }
+
+    private void handlePlayerList(ByteArrayDataInput in) {
+        String server = in.readUTF();
+        String[] playerList = in.readUTF().split(", ");
+        PLAYER_LIST.put(server, new PlayerList(server, playerList, playerList.length));
+    }
+
+    private void handleServerIP(ByteArrayDataInput in) {
+        String server = in.readUTF();
+        String ip = in.readUTF();
+        int port = in.readUnsignedShort();
+        SERVER_INFO.put(server, new ServerInfo(server, ip, port));
+    }
+
+    public static record ServerInfo(String server, String ip, int port) {}
+    public static record PlayerList(String server, String[] playerList, int playerCount) {}
 }
